bbde0ca67b3cc75f859ba8dfc306b302cdd5d220
thechutrain <chu.chuyalan@gmail.com>
2016-12-04 23:07:58 -0500

    forms&events - made a form in the body.html and body.js contains an event handler for when someone enters input into form & also now returns the tasks as the most recent items

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -5,6 +5,29 @@ import './body.html';
 
 Template.body.helpers({
   tasks(){
-    return Tasks.find({});
+    return Tasks.find({}, { sort: {createdAt: -1} });
   }
+});
+
+Template.body.events({
+  'submit .new-task'(event){
+  //^ looks like a CSS selector but a string!
+    // prevent the default refresh
+    event.preventDefault();
+
+    // get values from the form
+    // console.log(event) // pretty big event!
+    const target = event.target;
+    const text = target.text.value;
+
+    // insert the task into a collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+    });
+
+    //clear form
+    target.text.value = " ";
+  },
+
 });
