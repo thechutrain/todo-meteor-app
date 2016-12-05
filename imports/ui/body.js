4a957238787edb6e164268e12ee2b348e2a6aad2
thechutrain <chu.chuyalan@gmail.com>
2016-12-05 11:00:00 -0500

    VII) Temporary UI state: added reactive-dict meteor package in order to keep track of UI state of whether to show completed tasks or not

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,30 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js'; // Tasks is the Mongo.Collection!
 
 import './body.html';
 
 Template.body.helpers({
   tasks(){
+    const instance = Template.instance(); //?? What does Template.instance() get? this??
+    if (instance.state.get('hideCompleted')){
+      return Tasks.find({completed: {$ne: true}}, {sort: {createdAt: -1} });
+    }
+
+    // otherwise return all
     return Tasks.find({}, { sort: {createdAt: -1} });
-  }
+  },
+  incompleteCount(){
+    return Tasks.find({completed: false}).count();
+  },
 });
+
+
+Template.body.onCreated(function bodyOnCreated(){
+  this.state = new ReactiveDict();
+});
+
 
 Template.body.events({
   'submit .new-task'(event){
@@ -26,9 +43,12 @@ Template.body.events({
       createdAt: new Date(),
       completed: false,
     });
-
     //clear form
     target.text.value = " ";
+  },
+
+  'change .hide-completed-tasks'(event, instance){
+    instance.state.set('hideCompleted', event.target.checked);
   },
 
 });
