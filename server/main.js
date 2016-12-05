f1b913e626f0bf285fe065f0113ed2cbd09c1c55
thechutrain <chu.chuyalan@gmail.com>
2016-12-04 22:26:06 -0500

    import tasks.js file which established the mongoDB collection

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
 
+// create the MongoDB collections - 'tasks'
+import '../imports/api/tasks.js';
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
